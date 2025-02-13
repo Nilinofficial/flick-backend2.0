49646771626b3469c4bd56e21ccebf65268d074b
Nilinofficial <itsmenilin18@gmail.com>
2025-02-13 21:19:36 +0530

    fix for deployment

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,25 @@ import { connectToDB } from './config/database';
 import requestRouter from './routes/request';
 import profileRouter from './routes/profile';
 import userRouter from './routes/user';
-
+import cors from 'cors';
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: true, // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Allow cookies and authentication headers
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/profile', profileRouter);
-app.use("/request",requestRouter);
-app.use("/user",userRouter)
+app.use('/request', requestRouter);
+app.use('/user', userRouter);
 
 connectToDB().then(() =>
   app.listen(PORT, () => {
